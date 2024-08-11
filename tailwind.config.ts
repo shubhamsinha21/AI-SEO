f5f25fad69cc2b0f70b401608d00dab93b720f99
rozer <shubhamsinha07084@gmail.com>
2024-08-11 14:26:29 +0530

    set-up structure of project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,17 +2,33 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    // defining configuration of container
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "20px",
+        lg: "80px",
       },
+      screens: {
+        sm: "375px",
+        md: "768px",
+        lg: "1200px",
+      },
+    },
+
+    // defining screens for responsiveness
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1200px",
+    },
+    extend: {
+      colors: {},
     },
   },
   plugins: [],
